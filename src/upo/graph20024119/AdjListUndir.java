c1d64d792794d9583590d40ecc9a041e9499dae9
Ziocash <31541164+Ziocash@users.noreply.github.com>
2022-10-20 18:08:26 +0200

    Update AdjListUndir.java
    [+] Overrided unsupported methods
    
    Co-Authored-By: GildartsClive98 <49634803+GildartsClive98@users.noreply.github.com>

--- src/upo/graph20024119/AdjListUndir.java
+++ src/upo/graph20024119/AdjListUndir.java
@@ -1,5 +1,19 @@
 package upo.graph20024119;
 
+import java.util.NoSuchElementException;
+
+import upo.graph.base.WeightedGraph;
+
 public class AdjListUndir extends AdjListUndirWeight {
 
+    @Override
+    public void setEdgeWeight(String arg0, String arg1, double arg2)
+            throws IllegalArgumentException, NoSuchElementException {
+        throw new UnsupportedOperationException("Non è possibile impostare il peso dell'arco su un grafo non pesato.");
+    }
+
+    @Override
+    public WeightedGraph getPrimMST(String arg0) throws UnsupportedOperationException, IllegalArgumentException {
+        throw new UnsupportedOperationException("Non è possibile generare un Minimo Albero Ricoprente su un grafo non pesato.");
+    }
 }
